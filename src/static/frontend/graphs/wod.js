93efec8cd1f71bb39b22171a6e5da8202a24d81b
Ellie Deveson <edev3158@uni.sydney.edu.au>
2025-06-03 16:04:52 +1000

    WOD Table
    Table for displaying WOD data.

--- src/static/frontend/graphs/wod.js
+++ src/static/frontend/graphs/wod.js
@@ -1,23 +1,38 @@
 // update
 function updateWOD(data) {
-    document.getElementById("batt-voltage").innerText = `Battery Voltage: ${data["bat-voltage"].toFixed(2)} V`;
-    document.getElementById("batt-current").innerText = `Battery Current: ${data["bat-current"].toFixed(2)} A`;
-    document.getElementById("3v3-current").innerText = `3.3V Current: ${data["3v3-current"].toFixed(2)} A`;
-    document.getElementById("5v-current").innerText = `5V Current: ${data["5v-current"].toFixed(2)} A`;
-    document.getElementById("comm-temp").innerText = `Comm Temp: ${data["comm-temp"].toFixed(1)} °C`;
-    document.getElementById("eps-temp").innerText = `EPS Temp: ${data["eps-temp"].toFixed(1)} °C`;
-    document.getElementById("batt-temp").innerText = `Battery Temp: ${data["batt-temp"].toFixed(1)} °C`;
+    document.getElementById("mode").innerText = `${data.mode}`;
+    document.getElementById("batt-voltage").innerText = `${data["bat-voltage"].toFixed(2)} V`;
+    document.getElementById("batt-current").innerText = `${data["bat-current"].toFixed(2)} A`;
+    document.getElementById("3v3-current").innerText = `${data["3v3-current"].toFixed(2)} A`;
+    document.getElementById("5v-current").innerText = `${data["5v-current"].toFixed(2)} A`;
+    document.getElementById("comm-temp").innerText = `${data["comm-temp"].toFixed(1)} °C`;
+    document.getElementById("eps-temp").innerText = `${data["eps-temp"].toFixed(1)} °C`;
+    document.getElementById("batt-temp").innerText = `${data["batt-temp"].toFixed(1)} °C`;
 }
 
 
 // Fetch latest WOD
 function fetchWODData() {
-    fetch('/api/wod_data')
+    fetch('/wod')
         .then(res => res.json())
         .then(data => {
-            updateWOD(data);
+            if (data.status === "success" && data.WOD_data) {
+                const values = data.WOD_data;
+
+                updateWOD({
+                    timestamp: values[0],
+                    mode: values[1],
+                    "bat-voltage": values[2],
+                    "bat-current": values[3],
+                    "3v3-current": values[4],
+                    "5v-current": values[5],
+                    "comm-temp": values[6],
+                    "eps-temp": values[7],
+                    "batt-temp": values[8]
+                });
+            }
         })
-        .catch(err => console.error("Error fetching data:", err));
+        .catch(err => console.error("Error fetching WOD data:", err));
 }
 
 setInterval(fetchWODData, 2000); // Fetch every 2 seconds
