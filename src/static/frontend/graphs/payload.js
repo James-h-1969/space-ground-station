e99bbbcc5fe6a019798dde471b962994c7f70499
Ellie Deveson <edev3158@uni.sydney.edu.au>
2025-06-04 15:24:36 +1000

    Small changes

--- src/static/frontend/graphs/payload.js
+++ src/static/frontend/graphs/payload.js
@@ -15,7 +15,7 @@ function plotSpectrums() {
         ctx1.font = "16px sans-serif";
         ctx1.textAlign = "center";
         ctx1.textBaseline = "middle";
-        ctx1.fillText("Set state as PAYLOAD to display results.", intensityCanvas.width / 2, intensityCanvas.height / 2);
+        ctx1.fillText("Set state as PAYLOAD to record & display results.", intensityCanvas.width / 2, intensityCanvas.height / 2);
 
         return;
     }
